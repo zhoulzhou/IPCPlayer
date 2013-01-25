fcc51595a62fbc226d1376c37b325d6aff436e81
whz <sohozhou@163.com>
2013-01-25 16:33:05 +0800

    add meths related to file and storage

---
+++ src/com/example/ipcplayer/utils/FileUtil.java
@@ -0,0 +1,32 @@
+package com.example.ipcplayer.utils;
+
+import java.io.File;
+
+import android.os.Environment;
+
+public class FileUtil{
+	public static String DIR_HOME = "Ipc_ready";
+	public static String DIR_MUSIC = "music";
+	private static String TAG = FileUtil.class.getSimpleName();
+	
+	public static String getStoragePath(){
+		LogUtil.d(TAG + " getStoragePath ");
+		if(StorageUtil.isExternalStorageAvailable()){
+			return Environment.getExternalStorageDirectory().getAbsolutePath();
+		}else {
+			return Environment.getDataDirectory().getAbsolutePath();
+		}
+	}
+	
+	public static File getIPCHomeDir(){
+		LogUtil.d(TAG + " getIPCHomeDir ");
+		return new File(getStoragePath(),DIR_HOME);
+	}
+	
+	public static File getIPCMusicDir(){
+		LogUtil.d(TAG + " getIPCMusicDir ");
+		return new File(getIPCHomeDir(),DIR_MUSIC);
+	}
+	
+	
+}
--- src/com/example/ipcplayer/utils/StorageUtil.java
+++ src/com/example/ipcplayer/utils/StorageUtil.java
@@ -1,5 +1,90 @@
 package com.example.ipcplayer.utils;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Environment;
+import android.os.StatFs;
+
 public class StorageUtil{
+	
+	   //¼àÌýsdcard×´Ì¬¹ã²¥  
+	   private BroadcastReceiver mExternalStorageReceiver;  
+	   //sdcard¿ÉÓÃ×´Ì¬  
+	   private static boolean mExternalStorageAvailable = false;  
+	   //sdcard¿ÉÐ´×´Ì¬  
+	   private static boolean mExternalStorageWriteable = false;  
+	   private static String TAG = StorageUtil.class.getSimpleName();
+	  
+	   public static void updateExternalStorageState() {  
+		   LogUtil.d(TAG + " updateExternalStorageState ");
+	       //»ñÈ¡sdcard¿¨×´Ì¬  
+	       String state = Environment.getExternalStorageState();  
+	       if (Environment.MEDIA_MOUNTED.equals(state)) {  
+	           mExternalStorageAvailable = mExternalStorageWriteable = true;  
+	       } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {  
+	           mExternalStorageAvailable = true;  
+	           mExternalStorageWriteable = false;  
+	       } else {  
+	           mExternalStorageAvailable = mExternalStorageWriteable = false;  
+	       }  
+	         
+	   }  
+	   
+	   public static boolean isExternalStorageAvailable(){
+		   LogUtil.d(TAG + " isExternalStorageAvailable ");
+		   String state = Environment.getExternalStorageState();
+		   if(Environment.MEDIA_MOUNTED.equals(state)){
+			   return true ;
+		   }else {
+			   return false ;
+		   }
+	   }
+	   
+	   public static StatFs getStatFs(){
+		   LogUtil.d(TAG + " getStatFs ");
+		   StatFs statFs ;
+		   if(isExternalStorageAvailable()){
+			   statFs = new StatFs(Environment.getExternalStorageDirectory().getPath());
+		   }else{
+			   statFs = new StatFs(Environment.getDataDirectory().getPath());
+		   }
+		   return statFs ;
+	   }
+	   
+	   public static long getTotalSpace(){
+		   LogUtil.d(TAG + " getTotalSpace ");
+		   long blockSize = getStatFs().getBlockSize();
+		   long totalCount = getStatFs().getBlockCount();
+		   return blockSize * totalCount;
+		}
+	   
+	   public static long getFreeSpace(){
+		   LogUtil.d(TAG + " getFreeSpace ");
+		   long blockSize = getStatFs().getBlockSize();
+		   long availableBlock = getStatFs().getAvailableBlocks();
+		   return blockSize * availableBlock;
+	   }
+	   
+	   //¿ªÊ¼¼àÌý  
+//	   void startWatchingExternalStorage() {  
+//	       mExternalStorageReceiver = new BroadcastReceiver() {  
+//	           @Override  
+//	           public void onReceive(Context context, Intent intent) {  
+//	               LogUtil.d(TAG + "Storage: " + intent.getData());  
+//	               updateExternalStorageState();  
+//	           }  
+//	       };  
+//	       IntentFilter filter = new IntentFilter();  
+//	       filter.addAction(Intent.ACTION_MEDIA_MOUNTED);  
+//	       filter.addAction(Intent.ACTION_MEDIA_REMOVED);  
+//	       registerReceiver(mExternalStorageReceiver, filter);  
+//	       updateExternalStorageState();  
+//	   }  
+	   //Í£Ö¹¼àÌý  
+//	   void stopWatchingExternalStorage() {  
+//	       unregisterReceiver(mExternalStorageReceiver);  
+//	   }  
 	
 }
