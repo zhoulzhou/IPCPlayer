a8fa8131a36ec3d1849a3203e6e4d0ec35863ef5
whz <sohozhou@163.com>
2013-04-12 11:15:28 +0800

    add delaytime

--- src/com/example/ipcplayer/activity/SplashActivity.java
+++ src/com/example/ipcplayer/activity/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.ipcplayer.activity;
 
 import com.example.ipcplayer.R;
 import com.example.ipcplayer.provider.MusicDBManager;
+import com.example.ipcplayer.utils.LogUtil;
 import com.example.ipcplayer.utils.StorageUtil;
 
 import android.app.Activity;
@@ -9,10 +10,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 
 public class SplashActivity extends Activity{
+	private static final String TAG = SplashActivity.class.getSimpleName();
 	private Context mContext;
 	private StartTask mLoadingTask;
+	private long mStartTime;
+	private static final long DELAY_TIME = 1000 ;
+	
+	private Handler mHandler = new Handler(){
+			
+			@Override
+			public void handleMessage(Message msg) {
+				super.handleMessage(msg);
+			}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,23 +38,25 @@ public class SplashActivity extends Activity{
 		setContentView(R.layout.welcome);
 		mContext = this;
 		
-		initActivity();
+		loadDataAndInitActivity();
 	}
 	
-	private void initActivity(){
+	private void loadDataAndInitActivity(){
 		mLoadingTask = new StartTask();
 		mLoadingTask.execute("loading local data ...");
 	}
 	
-	private void showHelporMain(){
-		if(shouldShowHelp()){
-			Intent intent = new Intent(this,HelpActivity.class);
-			mContext.startActivity(intent);
-		}else{
-			Intent intent = new Intent(this,MainActivity.class);
-			mContext.startActivity(intent);
+	private void showHelporMain(boolean result){
+		if (result) {
+			if (shouldShowHelp()) {
+				Intent intent = new Intent(this, HelpActivity.class);
+				mContext.startActivity(intent);
+			} else {
+				Intent intent = new Intent(this, MainActivity.class);
+				mContext.startActivity(intent);
+			}
+			finish();
 		}
-		finish();
 	}
 	
 	private boolean shouldShowHelp(){
@@ -60,18 +76,43 @@ public class SplashActivity extends Activity{
 	private class StartTask extends AsyncTask<String, Void, Boolean>{
 
 		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			mStartTime = System.currentTimeMillis();
+			LogUtil.d(TAG + " start loading data time: " + mStartTime);
+		}
+
+		@Override
 		protected Boolean doInBackground(String... arg0) {
+			LogUtil.d(TAG + " loading data ...");
 			if (StorageUtil.isExternalStorageAvailable()) {
 				MusicDBManager.getInstance(mContext).insertLocalData();
+				return true;
 			}
-			return true;
+			return false;
 		}
 
 		@Override
 		protected void onPostExecute(Boolean result) {
 			super.onPostExecute(result);
-			
-			showHelporMain();
+			LogUtil.d(TAG + " loading data result: " + result.toString());
+			final boolean loadResult = result;
+			long taskTime = System.currentTimeMillis() - mStartTime;
+			LogUtil.d(TAG + " loading data needs time: " + taskTime);
+			long delayTime = DELAY_TIME - taskTime;
+			LogUtil.d(TAG + " delayTime: " + delayTime);
+			if (delayTime > 0) {
+				mHandler.postDelayed(new Runnable(){
+
+					@Override
+					public void run() {
+						showHelporMain(loadResult);
+					}
+					
+				}, delayTime);
+			}else {
+				showHelporMain(loadResult);
+			}
 		}
 		
 	}
