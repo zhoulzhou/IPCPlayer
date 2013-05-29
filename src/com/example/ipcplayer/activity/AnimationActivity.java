1d42502ace501d8555999cea56ba4572716755d0
whz <sohozhou@163.com>
2013-05-29 15:12:27 +0800

    rotate and translate animation

--- src/com/example/ipcplayer/activity/AnimationActivity.java
+++ src/com/example/ipcplayer/activity/AnimationActivity.java
@@ -1,6 +1,7 @@
 package com.example.ipcplayer.activity;
 
 import com.example.ipcplayer.R;
+import com.example.ipcplayer.animation.RotateAndTranslateAnimation;
 
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
@@ -24,15 +25,17 @@ public class AnimationActivity extends Activity implements OnClickListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.animation1);
 		anima = (ImageView) findViewById(R.id.myAnimation);
-		anima.setBackgroundResource(R.drawable.welcome_1);
+		anima.setBackgroundResource(R.drawable.runner);
 		start = (Button) findViewById(R.id.start);
 		stop = (Button) findViewById(R.id.stop);
 		
 //		anima.setBackgroundResource(R.anim.anima);
 //		animaD = (AnimationDrawable) anima.getBackground();
 		
-		mAnima = new AnimationUtils().loadAnimation(this, R.anim.rotate_anima);
+//		mAnima = new AnimationUtils().loadAnimation(this, R.anim.rotate_anima);
 		
+		mAnima = new RotateAndTranslateAnimation(0, 300, 0, 300, 0, 300);
+		mAnima.setDuration(5000);
 		
 		start.setOnClickListener(this);
 		stop.setOnClickListener(this);
